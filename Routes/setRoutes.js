ffda3bbda0c35a3a495858e86045ae48fc68e89f
lauro-vitor <laurovpina@gmail.com>
2020-09-20 19:05:15 -0300

    commit 2009

--- Routes/setRoutes.js
+++ Routes/setRoutes.js
@@ -6,6 +6,7 @@ router.post('/', (req, res) => {
     if(array) {
         setController.addNumbers(req.body.array);
         res.status(201).json(setController.getSet());
+        return;
     }
     res.status(400).json({
         message: 'Requisição Inválida, deve ser um JSON com um campo denominado array'
